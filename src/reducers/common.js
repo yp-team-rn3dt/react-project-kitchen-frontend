e05b3018940d798695efabf4b9758f99c910756c
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2022-05-28 18:37:42 +0300

    style[SP-6]: Стилизован баннер

--- src/reducers/common.js
+++ src/reducers/common.js
@@ -18,7 +18,7 @@ import {
 } from '../constants/actionTypes';
 
 const defaultState = {
-  appName: 'Practicum Project Kitchen',
+  appName: 'Проектная кухня',
   token: null,
   viewChangeCounter: 0,
 };
